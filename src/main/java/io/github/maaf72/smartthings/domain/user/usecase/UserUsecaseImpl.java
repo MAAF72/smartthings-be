17ae098eced1d9662865e15fee1a2c54e5ca3033
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2025-05-15 19:31:30 +0700

    feat: implement user usecase

--- src/main/java/io/github/maaf72/smartthings/domain/user/usecase/UserUsecaseImpl.java
+++ src/main/java/io/github/maaf72/smartthings/domain/user/usecase/UserUsecaseImpl.java
@@ -4,44 +4,123 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import io.github.maaf72.smartthings.domain.auditlog.repository.AuditLogRepository;
+import io.github.maaf72.smartthings.domain.common.dto.PaginationRequest;
 import io.github.maaf72.smartthings.domain.device.repository.DeviceRepository;
-import io.github.maaf72.smartthings.domain.user.dto.Filter;
+import io.github.maaf72.smartthings.domain.user.dto.LoginRequest;
+import io.github.maaf72.smartthings.domain.user.dto.RegisterRequest;
 import io.github.maaf72.smartthings.domain.user.entity.User;
+import io.github.maaf72.smartthings.domain.user.entity.User.Role;
+import io.github.maaf72.smartthings.domain.user.entity.UserWithTotalRegisteredDevices;
 import io.github.maaf72.smartthings.domain.user.repository.UserRepository;
-import io.github.maaf72.smartthings.itf.AppUsecaseItf;
+import io.github.maaf72.smartthings.infra.exception.HttpException;
+import io.github.maaf72.smartthings.infra.security.HashUtil;
+import io.github.maaf72.smartthings.infra.security.JwtUtil;
+import io.github.maaf72.smartthings.infra.security.UserClaims;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 @ApplicationScoped
-public class UserUsecaseImpl implements AppUsecaseItf, UserUsecase {
+public class UserUsecaseImpl implements UserUsecase {
 
   @Inject
-  UserRepository userRepository;
-  
+  UserRepository repository;
+
   @Inject
   DeviceRepository deviceRepository;
 
-  @Override
-  public Optional<User> findByID(UUID id) {
-    return userRepository.findByID(id);
+  @Inject
+  AuditLogRepository auditLogRepository;
+
+  public User getUser(UUID actorId, Role role, UUID userId) {
+    boolean isAllowed = 
+      (false)
+      || (role.equals(Role.ST_USERS) && actorId.equals(userId))
+      || (role.equals(Role.ST_ADMINISTRATOR));
+      
+    if (!isAllowed) {
+      throw new HttpException(403, "you are not allowed to view this user");
+    }
+
+    User user = repository.findById(userId).orElseThrow(() -> new HttpException(404, "user not found"));
+
+    // add preload devices
+    return user;
   }
 
-  @Override
-  public List<User> find(Filter filter) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'find'");
-  }
+  public List<UserWithTotalRegisteredDevices> listUserWithTotalRegisteredDevices(UUID actorId, Role role, PaginationRequest page) {
+    boolean isAllowed = (false)
+      || (role.equals(Role.ST_ADMINISTRATOR));
 
-  @Override
-  public void update(User user) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'update'");
-  }
+    if (!isAllowed) {
+      throw new HttpException(403, "you are not allowed to view users data");
+    }
 
-  @Override
-  public void delete(User user) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    return repository.findAllUserWithTotalRegisteredDevices(page);
   }
   
+  public long countUsers() {
+    return repository.countAllUser();
+  }
+ 
+  public List<UserWithTotalRegisteredDevices> listVendorWithTotalRegisteredDevices(UUID actorId, Role role, PaginationRequest page) {
+    boolean isAllowed = (false)
+      || (role.equals(Role.ST_ADMINISTRATOR));
+
+    if (!isAllowed) {
+      throw new HttpException(403, "you are not allowed to view vendors data");
+    }
+
+    return repository.findAllVendorWithTotalRegisteredDevices(page);
+  }
+  
+  public long countVendors() {
+    return repository.countAllVendor();
+  }
+ 
+  public User register(RegisterRequest request) {
+    Optional<User> userOpt = repository.findByEmail(request.getEmail());
+
+    if (userOpt.isPresent()) {
+      throw new HttpException(400, "email has been taken");
+    }
+
+    String hash = HashUtil.hash(request.getPassword());
+
+    User user = new User();
+    user.setEmail(request.getEmail());
+    user.setName(request.getName());
+    user.setHash(hash);
+    user.setDateOfBirth(request.getDateOfBirth());
+    user.setAddress(request.getAddress());
+    user.setCountry(request.getCountry());
+    user.setRole(Role.ST_USERS);
+
+    user = repository.create(user);
+
+    return user;
+  }
+  
+  public String login(LoginRequest request) {
+    Optional<User> userOpt = repository.findByEmail(request.getEmail());
+
+    User user = userOpt.orElseThrow(() -> new HttpException(401, "invalid credentials"));
+
+    if (!HashUtil.verify(request.getPassword(), user.getHash())) {
+      throw new HttpException(401, "invalid credentials");
+    }
+
+    UserClaims claimsObj = new UserClaims();
+    
+    claimsObj.setId(user.getId());
+    claimsObj.setEmail(user.getEmail());
+    claimsObj.setName(user.getName());
+    claimsObj.setRole(user.getRole());
+
+    String token = JwtUtil.generateJWTToken(user);
+
+    return token;
+  }
+
 }
+    
