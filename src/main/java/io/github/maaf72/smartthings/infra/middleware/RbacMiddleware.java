096a594d318e80341e27a86053983ec913ec9500
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2025-05-16 08:03:08 +0700

    fix: remove unused annotation

--- src/main/java/io/github/maaf72/smartthings/infra/middleware/RbacMiddleware.java
+++ src/main/java/io/github/maaf72/smartthings/infra/middleware/RbacMiddleware.java
@@ -3,11 +3,9 @@ package io.github.maaf72.smartthings.infra.middleware;
 import io.github.maaf72.smartthings.domain.user.entity.User.Role;
 import io.github.maaf72.smartthings.infra.security.UserClaims;
 import io.github.maaf72.smartthings.itf.AppMiddlewareItf;
-import jakarta.enterprise.context.ApplicationScoped;
 import lombok.RequiredArgsConstructor;
 import ratpack.core.handling.Context;
 
-@ApplicationScoped
 @RequiredArgsConstructor
 public class RbacMiddleware implements AppMiddlewareItf {
   private final Role accessRole;
--- src/main/java/io/github/maaf72/smartthings/infra/thirdapi/translation/TranslationServiceImpl.java
+++ src/main/java/io/github/maaf72/smartthings/infra/thirdapi/translation/TranslationServiceImpl.java
@@ -5,22 +5,10 @@ import java.util.List;
 
 import io.github.maaf72.smartthings.infra.thirdapi.translation.dto.TranslationResponse;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Produces;
 
 @ApplicationScoped
 public class TranslationServiceImpl implements TranslationService {
   private static final String[] LIST_COUNTRY_ID = { "en", "id", "my", "jp" };
-
-  private static final TranslationService transaltorService = buildTranlationService();
-
-  private static TranslationService buildTranlationService() {
-    return new TranslationServiceImpl();
-  }
-  
-  @Produces
-  public static TranslationService getTranslationService() {
-    return transaltorService;
-  }
 
   public String SingleCountryTranslate(String text, String countryID) {
     return "[%s] %s".formatted(countryID, text);
